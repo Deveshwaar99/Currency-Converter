976d1e5657ac44c64c240dd9705dcdf7a1645746
Deveshwaar <deveshwaar99@gmail.com>
2023-05-31 12:19:54 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Convert from "./components/Convert"
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Convert />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./styles.css"
+
+export default function Header() {
+  return (
+    <>
+      <div className="header">
+        <h2>Convertor</h2>
+      </div>
+      <div className="date">
+        <h1>Saturday,20th May,2023,Week 20</h1>
+      </div>
+    </>
+  )
+}
